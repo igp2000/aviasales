e30c45ec83699f5dcacadb938106a60108b0793a
igp2000 <74302129+igp2000@users.noreply.github.com>
2022-05-18 11:18:04 +0400

    Delete src directory

--- src/components/Airlines/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Airlines } from './Airlines';
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-import reduser from './reducer';
-
-const store = createStore(reduser, applyMiddleware(thunk));
-
-export default store;
